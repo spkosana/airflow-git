fd547a7c3746b40574085648cc9e7bcf33c23913
Surya Kosana <spkosana.82@gmail.com>
2019-12-07 15:22:46 -0500

    learned about setup tools and testing the packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="airflow dags package", 
+    version="0.1", 
+    description="This is test airflow package",
+    long_description="Surya Kosana is learning how to create airflow package",
+    author="Surya Kosana", 
+    packages=['dags'],
+    install_requires=[]
+)
